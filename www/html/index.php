d47c8565cb459a9106115e979bd7544956c1aeae
troyg <troy.gidney@gmail.com>
2022-12-12 22:27:01 -0700

    Switching over to docker for  ease of use

---
+++ www/html/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>  
+     <head>  
+      <title>Hello World!</title>
+     </head>   
+
+     <body>  
+      <h1>Hello World!</h1>  
+      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>  
+      <?  
+       $database ="data";  
+       $user = "root";  
+       $password = "Qwaszx1230";  
+       $host = "localhost";  
+
+       $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);  
+       $query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");  
+       $tables = $query->fetchAll(PDO::FETCH_COLUMN);  
+
+        if (empty($tables)) {
+          echo "<p>There are no tables in database \"{$database}\".</p>";
+        } else {
+          echo "<p>Database \"{$database}\" has the following tables:</p>";
+          echo "<ul>";
+            foreach ($tables as $table) {
+              echo "<li>{$table}</li>";
+            }
+          echo "</ul>";
+        }
+        ?>
+    </body>
+</html>
