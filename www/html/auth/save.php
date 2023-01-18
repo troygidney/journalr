d6e0681a83d4b0dbef7847949d2c1a589f29dd68
troyg <troy.gidney@gmail.com>
2023-01-17 21:54:05 -0700

    Adding 403 for GET request to save

--- www/html/auth/save.php
+++ www/html/auth/save.php
@@ -3,6 +3,10 @@ require_once '/var/www/html/vendor/autoload.php';
 require '/var/www/php/auth/LoginService.php';
 require '/var/www/php/auth/SaveData.php';
 
+if ($_SERVER['REQUEST_METHOD'] != "POST") {
+    http_response_code(403);
+    exit;
+}
 
 session_start();
 
